73baedc298a76fa9f877cf775149cb26a2434631
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-02-19 04:40:36 -0700

    Adding helper method to get logger

--- src/controller.ts
+++ src/controller.ts
@@ -1,5 +1,11 @@
 import Router from '@koa/router';
+import {Logger, getLogger} from '@nr1e/logging';
+import {Context} from 'koa';
 
 export abstract class Controller {
+  protected getLogger(ctx: Context, name: string): Logger {
+    return getLogger(name, ctx.logger);
+  }
+
   abstract registerRoutes(router: Router): void;
 }
